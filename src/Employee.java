13e9d6ea35a71ad762047679e8b31d0cd4df1db6
oyqh <ouyangqinghua123@outlook.com>
2023-10-08 20:06:35 +0800

    initial project version

---
+++ src/Employee.java
@@ -0,0 +1,48 @@
+import java.time.LocalDate;
+
+public class Employee {
+    private final int id;
+    private static int nextId = 1;
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+    
+    public static int getNextID()
+    {
+        return nextId;
+    } 
+
+    public Employee(String name,double salary,int year,int month,int day)
+    {
+        id = nextId++;
+        this.name = name;
+        this.salary = 1200.00+salary;//底薪1200
+        hireDay = LocalDate.of(year,month,day);
+    }
+    public Employee()
+    {
+        id = nextId++;
+        this.name = "none";
+        this.salary = 1200.00;
+        hireDay = LocalDate.of(2000,1,1);
+    }
+    
+
+    public int getId()
+    {
+        return id;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public double getSalary()
+    {
+        return salary;
+    }
+    public LocalDate getHireDay()
+    {
+        return hireDay;
+    }
+
+}
---
+++ src/Engineer.java
@@ -0,0 +1,24 @@
+public class Engineer extends Employee{
+    private double bonus;
+
+    public Engineer(String name,double bonus, double salary, int year, int month, int day)
+    {
+        super(name, salary, year, month, day);
+        this.bonus = bonus;
+    }
+    public Engineer(){}
+
+    public double getTotalsalaries()
+    {
+        return super.getSalary() + this.bonus;
+    } 
+    public static void main(String[] args)
+    {
+        Employee[] staff = new Employee[3];
+        staff[0] = new Engineer();
+        staff[1] = new Employee();
+        staff[2] = new Engineer();
+        for(Employee i : staff)
+        System.out.println(i.getId());
+    }
+}
